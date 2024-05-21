83a2ada6f14765e013d2741874925e428f969c90
Aditya Saini <aditya.saini7b2024@gmail.com>
2024-05-21 09:53:35 +0530

    Add files via upload

---
+++ Contact Book.py
@@ -0,0 +1,21 @@
+# Contact Book.py
+
+contact_name = []
+name = input("Name:")
+
+contact_name.append(name)
+print (contact_name)
+
+contact_number = []
+number = int(input("Mobile Number:"))
+
+contact_number.append(number)
+print (contact_number)
+
+contact_mail = []
+mail = input("Mail:")
+
+contact_mail.append(mail)
+print (contact_mail)
+
+
